c2e23cf453d9554f20921e388f93799080074a0b
ufuk2606 <ufukozkar11@gmail.com>
2023-06-15 04:35:20 +0200

    dashboard user to get and to update

--- src/controller/dashboard-route.js
+++ src/controller/dashboard-route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dasboardRepository from "../repository/dashboard-repository.js";
 import monatsHitsRepository from "../repository/monatsHits-repository.js";
+import userRepository from "../repository/user-repository.js";
 
 const router = express.Router();
 
@@ -25,6 +26,24 @@ router.post("/monatsHits", async (req, res, next) => {
     const essen = req.body;
     await monatsHitsRepository.addEssen( essen );
     return res.status(201).send(essen);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).send({ message: "Invalid user input" });
+    } else if (error.message === "User with this email already exists") {
+      return next({ message: "A user with this email already exists" });
+    } else {
+      return next(error);
+    }
+  }
+});
+
+router.put("/dashboard", async (req, res, next) => {
+  try {
+    const userEmail = req.query.email;
+    const updatedUser = req.body;
+    const user = await userRepository.getUserByEmail(userEmail);
+    await userRepository.updateUser(user.id, updatedUser);
+    return res.status(201).send(rezervation);
   } catch (error) {
     if (error.name === "ValidationError") {
       return res.status(400).send({ message: "Invalid user input" });
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,33 +1,32 @@
-import User from "../model/user-model.js"
+import User from "../model/user-model.js";
 
 const getUsers = async () => {
   try {
-    const users = await User.findAll()
-    return users
+    const users = await User.findAll();
+    return users;
   } catch (error) {
-    throw new Error('error while getting users');
+    throw new Error("error while getting users");
   }
-}
+};
 
 const getUserByEmail = async (pEmail) => {
   try {
     const user = await User.findOne({
       where: {
-        email: pEmail
-      }
-    })
+        email: pEmail,
+      },
+    });
     return user;
   } catch (error) {
-    throw new Error('error while getting users');
+    throw new Error("error while getting users");
   }
-
-}
+};
 
 const createUser = async (pUser) => {
   try {
     const existingUser = await User.findOne({ where: { email: pUser.email } });
     if (existingUser) {
-      throw new Error('User with this email already exists');
+      throw new Error("User with this email already exists");
     }
     const newUser = await User.create(pUser);
     return newUser;
@@ -37,19 +36,46 @@ const createUser = async (pUser) => {
   }
 };
 
+const updateUser = async (pId, pUser) => {
+  try {
+    const existingUser = await User.findOne({ where: { id: pId } });
+    if (!existingUser) {
+      throw new Error("There is no user with this id");
+    }
+    const newUser = await User.update(
+      {
+        firstName: pUser.firstName,
+        lastName: pUser.lastName,
+        email: pUser.email,
+        telefon: pUser.telefon,
+        street: pUser.street,
+        place: pUser.place,
+      },
+      {
+        where: {
+          id: pId,
+        },
+      }
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 const deleteUserById = async (pUserId) => {
   return await User.destroy({
     where: {
-      id: pUserId
-    }
+      id: pUserId,
+    },
   });
-}
+};
 
 const updatedUsers = async (users) => {
   try {
     const result = await User.bulkCreate(users, {
-      updateOnDuplicate: ["role"]
+      updateOnDuplicate: ["role"],
     });
     console.log(result); // Prints the updated rows
   } catch (error) {
@@ -57,11 +83,11 @@ const updatedUsers = async (users) => {
   }
 };
 
-
 export default {
   getUsers,
   createUser,
   deleteUserById,
   getUserByEmail,
-  updatedUsers
-}
+  updatedUsers,
+  updateUser,
+};
